5c70c1afdd5e029e137e00bc6f5fb6b9e1ce7483
Szymon Pawlak <Szymon Pawlak>
2017-02-05 23:12:23 +0100

    contact remove to gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,25 @@
 $(document).ready(function(){
 	$(".dg.ac").remove();
 	$(" div > div").remove(); //nasted fix TODO: change selector;
+	
 	$(".go_to_gallery").click(function() {
 		$("body").addClass("overlay");
 		setTimeout(function(){
 			window.location.href = 'assets/html/gallery.html';
 		}, 2000);
 	})
+	
 	$(".go_to_contact").click(function() {
 		$("body").addClass("overlay");
 		setTimeout(function(){
-			window.location.href = 'assets/html/about.html';
+			window.location.href = './about.html';
+		}, 2000);
+	})
+
+	$(".go_to_main").click(function() {
+		$("body").addClass("overlay");
+		setTimeout(function(){
+			window.location.href = '../../index.html';
 		}, 2000);
 	})
 });
