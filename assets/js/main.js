a35bd3fe620c0d8011f70d163ff4b9368001df85
Szymon Pawlak <Szymon Pawlak>
2017-02-05 20:30:59 +0100

    added overlay after change page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
 	$(".dg.ac").remove();
 	$(" div > div").remove(); //nasted fix TODO: change selector;
+	$(".go_to_gallery").click(function() {
+		$("body").addClass("overlay");
+		setTimeout(function(){
+			window.location.href = 'assets/html/gallery.html';
+		}, 2000);
+	})
+	$(".go_to_contact").click(function() {
+		$("body").addClass("overlay");
+		setTimeout(function(){
+			window.location.href = 'assets/html/about.html';
+		}, 2000);
+	})
 });
